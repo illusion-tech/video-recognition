534db0bb4ba693242033f675c8bfbf46e4e8840e
Xu Chao <xuchao@illusiontech.cn>
2023-12-19 16:07:40 +0800

    feat: api 支持单文件与多文件识别

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,10 @@
-FROM python:3.8
+FROM python:3.10
 
-LABEL user="Xu Chao"
-LABEL email="xuchao@illusiontech.cn"
-LABEL version="1.0"
-LABEL description="基于python3.8的FastAPI镜像"
-ENV DEBIAN_FRONTEND noninteractive
-
-RUN apt-get update -y \
-    && apt-get install -y --no-install-recommends \
-        git \
-    && apt-get clean \
-    && rm -r /var/lib/apt/lists/*
-
-#  国内源
-RUN pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
-# RUN pip3 config set global.index-url http://mirrors.cloud.tencent.com/pypi/simple
-# RUN pip3 config set global.index-url http://pypi.douban.com/simple/
-# RUN pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-
-# install fastapi
 COPY ./requirements.txt /
-RUN pip3 --no-cache-dir install -r /requirements.txt \
-    && rm -f /requirements.txt
+RUN pip install -r requirements.txt 
 
-# 终端设置
-# 默认值是dumb，这时在终端操作时可能会出现：terminal is not fully functional
-ENV LANG C.UTF-8
-ENV TERM xterm
-ENV PYTHONIOENCODING utf-8
+EXPOSE 3001
 
-# 解决时区问题
-ENV TZ "Asia/Shanghai"
+COPY . .
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "3001"]
---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+import cv2
+import uuid
+import numpy as np
+from typing import List
+from ultralytics import YOLO
+from datetime import datetime
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse, JSONResponse
+
+model = YOLO("models/best.pt")
+app = FastAPI()
+
+@app.post("/detect")
+async def predict(file: UploadFile = File(...)):
+    try:
+        image_bytes = await file.read()
+        image = np.frombuffer(image_bytes, dtype=np.uint8)
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+        result = model(image)
+
+        date = datetime.now().strftime("%Y%m%d")
+        predict_path = os.path.join("data/predict/", date)
+        if not os.path.exists(predict_path):
+            os.makedirs(predict_path)
+        filename = str(uuid.uuid4()) + '.jpg'
+        predict_path = os.path.join(predict_path, filename)
+        img = result[0].plot(font="Arial", line_width=2, font_size=15)
+        cv2.imwrite(predict_path, img)
+        return FileResponse(predict_path, media_type='image/jpeg')
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
+
+
+@app.post("/detect/multiple")
+async def predict_multiple(files: List[UploadFile] = File(...)):
+    try:
+        response_files = []
+        for file in files:
+            image_bytes = await file.read()
+            image = np.frombuffer(image_bytes, dtype=np.uint8)
+            image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+            result = model(image)
+        
+            date = datetime.now().strftime("%Y%m%d")
+            predict_path = os.path.join("data/predict/", date)
+            if not os.path.exists(predict_path):
+                os.makedirs(predict_path)
+            filename = str(uuid.uuid4()) + '.jpg'
+            predict_path = os.path.join(predict_path, filename)
+            img = result[0].plot(font="Arial", line_width=2, font_size=15)
+            cv2.imwrite(predict_path, img)
+
+            response_files.append(predict_path)
+        
+        responses = [FileResponse(file_path, media_type='image/jpeg') for file_path in response_files]
+        return responses
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
