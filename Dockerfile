50790dcf9d57a8f1ad9ecdaca838767ad5ecde18
Xu Chao <xuchao@illusiontech.cn>
2023-12-21 16:43:49 +0800

    fix: 采用sam方式进行图片识别

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python:3.10
+FROM python:3.10.12
 
 COPY ./requirements.txt /
-RUN pip install -r requirements.txt 
+RUN pip install -r requirements.txt -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 
 EXPOSE 3001
 
--- main.py
+++ main.py
@@ -7,18 +7,31 @@ from ultralytics import YOLO
 from datetime import datetime
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse, JSONResponse
+from ultralytics.models.sam import Predictor as SAMPredictor
 
-model = YOLO("models/best.pt")
+
+# model = YOLO("models/sam_b.pt")
 app = FastAPI()
 
 @app.post("/detect")
-async def predict(file: UploadFile = File(...)):
+async def predict(type: int, file: UploadFile = File(...)):
     try:
         image_bytes = await file.read()
         image = np.frombuffer(image_bytes, dtype=np.uint8)
         image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-        result = model(image)
+        # 创建 SAMPredictor
+        overrides = dict(conf=0.25, task='segment', mode='predict', imgsz=1024, model="models/sam_b.pt")
+        predictor = SAMPredictor(overrides=overrides)
+
+        # 设置图像
+        predictor.set_image(image)  # 使用图像文件设置
+        bboxes = [[226, 78, 344, 320], [347, 79, 470, 336]] if type==0 else [0, 180, 955, 536] if type==1 else [283, 150, 634, 538]
+        result = predictor(bboxes=bboxes)
+        # 重置图像  
+        predictor.reset_image()
+
+        # result = model(image)
 
         date = datetime.now().strftime("%Y%m%d")
         predict_path = os.path.join("data/predict/", date)
